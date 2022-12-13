8e31b0224f0efebdfba2ed818d1c6987e4da3279
Patrick <pto1026@gmail.com>
2022-12-12 19:26:37 -0700

    quick

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,19 +1,19 @@
-import Sequelize from 'sequelize'
-
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
-    const sequelize = new Sequelize('hangman', 'root', 'password', {
-        host: 'localhost',
-        dialect: 'mysql'
-    });
-    event.locals.database = sequelize;
-
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-
-    return await resolve(event);
-}
+// import Sequelize from 'sequelize'
+//
+// /** @type {import('@sveltejs/kit').Handle} */
+// export async function handle({ event, resolve }) {
+//     const sequelize = new Sequelize('hangman', 'root', 'password', {
+//         host: 'localhost',
+//         dialect: 'mysql'
+//     });
+//     event.locals.database = sequelize;
+//
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//     }
+//
+//     return await resolve(event);
+// }
